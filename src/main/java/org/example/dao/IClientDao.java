a813e10bdae6b4f943ece737cfdb220de03bf92b
Hugo <huguitronic17@gmail.com>
2024-11-06 21:10:15 +0100

    CLientDao hecho

--- src/main/java/org/example/dao/IClientDao.java
+++ src/main/java/org/example/dao/IClientDao.java
@@ -2,13 +2,14 @@ package org.example.dao;
 
 import org.example.model.Client;
 
+import java.sql.SQLException;
 import java.util.List;
 
 public interface IClientDao {
-    int insert(Client tocreate);
+    int insert(Client tocreate) throws SQLException;
     boolean update(Client toUpdate);
-    boolean delete(Client idtodelete);
-    int incrementPurchase(int amount);
+    boolean delete(int idtodelete);
+    boolean incrementPurchase(int clientid, int amount);
     Client getById(int clientid);
     List<Client> getAll();
     Client getByemail(String email);
