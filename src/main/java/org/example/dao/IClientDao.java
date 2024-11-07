2214622c3ba8e0f138fe930bbcc1762de133bb77
HugoAgustinZazo <huguitronic17@gmail.com>
2024-11-07 10:05:22 +0100

    dia2

--- src/main/java/org/example/dao/IClientDao.java
+++ src/main/java/org/example/dao/IClientDao.java
@@ -7,8 +7,8 @@ import java.util.List;
 public interface IClientDao {
     int insert(Client tocreate);
     boolean update(Client toUpdate);
-    boolean delete(Client idtodelete);
-    int incrementPurchase(int amount);
+    boolean delete(int idtodelete);
+    boolean incrementPurchase(int clientid,int amount);
     Client getById(int clientid);
     List<Client> getAll();
     Client getByemail(String email);
